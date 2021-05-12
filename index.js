69450866cba1114263388fe692b069f66166cb86
guideanalytics-dev <81708173+guideanalytics-dev@users.noreply.github.com>
2021-05-12 15:40:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use('/', require('./src/main.js'));
+const main = require('./src/main.js'));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
@@ -11,3 +11,5 @@ app.listen(port, () => {
 
 exports.app = functions.http.onRequest(app);
 module.exports = { app };
+
+main();
