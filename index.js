b9d8a195c59f4cbc9ce1efe0ebdfc999a4a5dc04
guideanalytics-dev <81708173+guideanalytics-dev@users.noreply.github.com>
2021-05-12 11:46:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use('/', require('./src/main'));
 
-app.get('/', (req, res) => {
-   res.send('Google Scraper');
-});
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`google-scraper: listening on port ${port}`);
